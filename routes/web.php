30d6475bb31be8d2f6026635203f813af8519f97
penggunakomputer10 <penggunakomputer10@gmail.com>
2022-11-09 17:30:00 +0700

    default login page

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\CMS\ReportController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::prefix('administrator')->group(function () {
